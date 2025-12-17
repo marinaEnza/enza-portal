d45cbfe531410dfb318cb4bdf67a75245a2b5a2e
marinaEnza <marinaEnza@users.noreply.github.com>
2025-12-17 14:31:02 +0000

    deploy: 49257c82e5f83127ac5c9747f71c2e98ab814b1e

---
+++ assets/Pagination-Cdi_qiXc-Gl2xeSlz.js
@@ -0,0 +1,12 @@
+import{G as n,a as e,K as i,R as r,S as o}from"./entry.client-CZ7fWKt4.js";/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],d=n("arrow-left",c);/**
+ * @license lucide-react v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],x=n("arrow-right",h),m=({prev:t,next:a,className:l})=>{const s="group transition-all p-5 space-x-1 rtl:space-x-reverse transition-all hover:text-foreground";return e.jsxs("div",{className:i("flex -mx-4 text-muted-foreground font-semibold",t?"justify-between":"justify-end",l),"data-pagefind-ignore":"all",children:[t&&e.jsx(r,{variant:"ghost",asChild:!0,children:e.jsxs(o,{to:t.to,relative:"path",className:s,children:[e.jsx(d,{size:14,strokeWidth:2.5}),e.jsx("span",{className:"text-lg truncate",children:t.label})]})}),a&&e.jsx(r,{variant:"ghost",asChild:!0,children:e.jsxs(o,{to:a.to,relative:"path",className:s,children:[e.jsx("span",{className:"text-lg truncate",children:a.label}),e.jsx(x,{size:14,strokeWidth:2.5})]})})]})};export{m as j};
+//# sourceMappingURL=Pagination-Cdi_qiXc-Gl2xeSlz.js.map
